7170d0c7d9c97d8d2840996960056e366fa3094f
Andrew Kane <andrew@ankane.org>
2023-02-03 19:07:49 -0800

    Transmute

--- src/os/windows/mod.rs
+++ src/os/windows/mod.rs
@@ -375,7 +375,7 @@ impl<T> ::std::ops::Deref for Symbol<T> {
         println!("deref");
         match self.pointer {
             // Additional reference level for a dereference on `deref` return value.
-            Some(f) => unsafe { &*(f as *const T) },
+            Some(f) => unsafe { std::mem::transmute(&f) },
             None => todo!(),
         }
     }
